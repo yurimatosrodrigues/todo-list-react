42d82eb6f5ea8ed7ed3f61dad0b18a3f1ef22b66
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2022-03-08 13:49:55 -0300

    Criação do projeto em react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  //let message = 'Hello World!'
+  const [message, setMessage] = useState('Hello World!');  
+  return(
+    <>
+      <div className="container">
+        {message}
+      </div>
+      <button onClick={()=> setMessage("teste")}>Mudar Mensagem</button>
+    </>    
+  )};
 
 export default App;
